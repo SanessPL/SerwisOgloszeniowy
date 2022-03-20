cade3902eade8ca9092eaa6c58aa2fde1661835a
spzla <patryk2k5.9909@gmail.com>
2022-03-20 21:26:34 +0100

    podstawowa struktura bazy danych

---
+++ DATABASE.sql
@@ -0,0 +1,22 @@
+DROP DATABASE IF EXISTS serwis_ogloszeniowy;
+
+CREATE DATABASE serwis_ogloszeniowy CHARACTER SET utf8 COLLATE utf8_polish_ci;
+
+USE serwis_ogloszeniowy;
+
+CREATE TABLE users (
+   id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+   username VARCHAR(32) NOT NULL UNIQUE,
+   email VARCHAR(255) NOT NULL UNIQUE,
+   first_name VARCHAR(60),
+   last_name VARCHAR(60),
+   password CHAR(60) NOT NULL,
+   created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+   last_seen_at DATETIME
+);
+
+CREATE TABLE offers (
+   id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+   user_id INT NOT NULL REFERENCES users(id),
+   price DECIMAL(10, 2)
+);
